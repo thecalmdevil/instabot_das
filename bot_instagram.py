d5d516e07eef6f828516b296ffb635b4c169fca2
thecalmdevil <kampvid@gmail.com>
2022-07-15 18:48:36 +0000

    1

--- bot_instagram.py
+++ bot_instagram.py
@@ -1,3 +1,4 @@
+from time import sleep
 from instabot import Bot
 
 my_bot = Bot()
@@ -17,3 +18,29 @@ my_bot.follow_users(["coding_for_beginners_,""python.app","py_beginners"])
 my_bot.upload_photo("pytube.jpg, caption=Pytube | Create your own youtube video downloader using python")
 
 #send message to user
+my_bot.send_message("Hello Steve!", "stevedias796")
+
+#like a post
+my_bot.like_user("stevedias796", amount=2)
+
+#comment
+user_id = my_bot.get_user_id_from_username("stevedias796")
+media_id = my_bot.get_last_user_medias#(user_id)
+my_bot.comment(media_id, "This is very nice. I like it!")
+
+sleep(20)
+#get list of followers of anyone
+followers_list = my_bot.get_user_followers("stevedias796")
+
+following_list = my_bot.get_user_following("stevedias796")
+
+for count, each_follower in enumerate(followers_list):
+    print(my_bot.get_username_from_user_id(each_follower))
+
+for each_follow in following_list:
+    if count > 4:
+        continue
+    sleep(5)
+    print(my_bot.get_username_from_user_id(each_follow))
+
+my_bot.logout()
